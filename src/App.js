b1f74cd1f76b34826e0620c4385b8a7644e06f12
Ali Faizan <alifaizan1983@gmail.com>
2023-01-19 23:04:39 +0530

    added some files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="headerName">building something for bois.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
