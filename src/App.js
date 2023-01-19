9a4ad401d882620e0efade43fb9437707352db30
Ali Faizan <alifaizan1983@gmail.com>
2023-01-19 23:44:38 +0530

    added line break in text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 
 function App() {
+  const text = 'building \n something \n for bois.';
+  let newText = text.split('\n').map(i => {
+    return <p>{i}</p>
+  })
   return (
     <>
-      <div>
-        <p className="headerName">building something for bois.</p>
+      <div className="App">
+      <p className="headerName">{newText}</p>
       </div>
     </>
   );
