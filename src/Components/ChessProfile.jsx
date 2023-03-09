b32996abbf28a1a3469394a963fe46e80752a784
Ali Faizan <alifaizan1983@gmail.com>
2023-03-09 17:31:39 +0530

    added ratig card

--- src/Components/ChessProfile.jsx
+++ src/Components/ChessProfile.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { IoCloseSharp } from "react-icons/io5";
+import { BsFillLightningFill } from "react-icons/bs";
+import { MdOutlineTimer } from "react-icons/md";
+import { GiBulletBill } from "react-icons/gi";
 
 export default function ChessProfile() {
   // DATA state
@@ -30,7 +33,14 @@ export default function ChessProfile() {
   }, []);
 
   // REQ on 2 diff param, and getting the data
-  function onClickingImage({avatar, name, username, last_online, league, player_id }) {
+  function onClickingImage({
+    avatar,
+    name,
+    username,
+    last_online,
+    league,
+    player_id,
+  }) {
     fetch(`https://api.chess.com/pub/player/${username}/stats`)
       .then((response) => response.json())
       .then((data) => {
@@ -45,11 +55,17 @@ export default function ChessProfile() {
           blitz_rating: data.chess_blitz.last.rating,
           bullet_rating: data.chess_bullet.last.rating,
           avatar: avatar,
-
         });
         setIsOpen(true);
       });
-    setSelectedPlayer({avatar, name, username, last_online, league, player_id });
+    setSelectedPlayer({
+      avatar,
+      name,
+      username,
+      last_online,
+      league,
+      player_id,
+    });
     setIsOpen(true);
   }
 
@@ -116,7 +132,18 @@ export default function ChessProfile() {
       {/* MODAL */}
       {selectedPlayer && (
         <div className="modal">
-      <img src={selectedPlayer.avatar} alt={selectedPlayer.username} width={100}/>
+          {selectedPlayer.avatar ? (
+            <img
+              src={selectedPlayer.avatar}
+              alt={selectedPlayer.username}
+              width={100}
+            />
+          ) : (
+            <img
+              src="https://via.placeholder.com/100x100.png?text=No+Avatar"
+              alt="No Avatar"
+            />
+          )}
           <h2 className="modal_playerName">
             <span>{selectedPlayer.username}</span>
           </h2>
@@ -134,39 +161,53 @@ export default function ChessProfile() {
             Last Online: <span>{online(selectedPlayer.last_online)}</span>
           </p>
           <hr></hr>
-          {selectedPlayer.blitz_rating ? (
-            <p>
-              Rapid Rating: <span>{selectedPlayer.rapid_rating}</span>
-            </p>
-          ) : (
-            <p>Rapid Rating: <span>0</span></p>
-          )}
+          <div className="chess_rating">
+            {selectedPlayer.blitz_rating ? (
+              <div className="rating">
+                <p className="rating">Blitz</p>
+                {<BsFillLightningFill color="#FEDD00" />}
+                <p className="rating">{selectedPlayer.blitz_rating}</p>
+              </div>
+            ) : (
+              <p className="rating">
+                Blitz Rating: <span>0</span>
+              </p>
+            )}
 
-          {selectedPlayer.blitz_rating ? (
-            <p>
-              Blitz Rating: <span>{selectedPlayer.blitz_rating}</span>
-            </p>
-          ) : (
-            <p>Blitz Rating: <span>0</span></p>
-          )}
+            {selectedPlayer.rapid_rating ? (
+              <div className="rating">
+                <p className="rating">Rapid</p>
+                {<MdOutlineTimer color="#6BF216" />}
+                <p className="rating">{selectedPlayer.rapid_rating}</p>
+              </div>
+            ) : (
+              <p className="rating">
+                Blitz Rating: <span>0</span>
+              </p>
+            )}
 
-          {selectedPlayer.bullet_rating ? (
-            <p>
-              Bullet Rating: <span>{selectedPlayer.bullet_rating}</span>
-            </p>
-          ) : (
-            <p>Bullet Rating: <span>0</span></p>
-          )}
+            {selectedPlayer.bullet_rating ? (
+              <div className="rating">
+                <p className="rating">Bullet</p>
+                {<GiBulletBill color="#F7F749" />}
+                <p className="rating">{selectedPlayer.bullet_rating}</p>
+              </div>
+            ) : (
+              <p className="rating">
+                Blitz Rating: <span>0</span>
+              </p>
+            )}
 
-          <button
-            className="modal_close_button"
-            onClick={() => {
-              setSelectedPlayer(null);
-              setIsOpen(false);
-            }}
-          >
-            {<IoCloseSharp size={20} />}
-          </button>
+            <button
+              className="modal_close_button"
+              onClick={() => {
+                setSelectedPlayer(null);
+                setIsOpen(false);
+              }}
+            >
+              {<IoCloseSharp size={20} />}
+            </button>
+          </div>
         </div>
       )}
     </>
