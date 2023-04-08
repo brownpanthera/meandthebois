e0fb45ac507347ab44e7ee4c5d0c7ddffd3477e7
Ali Faizan <alifaizan1983@gmail.com>
2023-04-08 16:35:29 +0530

    fixed the gallery

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function Gallery() {
   const [selectedImage, setSelectedImage] = useState(null);
@@ -10,19 +10,39 @@ export default function Gallery() {
   return (
     <div>
       <>
-        <div className='photo-grid'>
-          <img className='images' src="/photos/bois1.jpg" alt='bois' onClick={handleClick} />
-          <img className='images' src="/photos/bois2.jpg" alt='bois' onClick={handleClick} />
-          <img className='images' src="/photos/bois3.jpg" alt='bois' onClick={handleClick} />
-          <img className='images' src="/photos/bois8.jpg" alt='bois' onClick={handleClick} />
+        <div className="photo-grid">
+          <img
+            className="images"
+            src="/photos/bois1.jpg"
+            alt="bois"
+            onClick={handleClick}
+          />
+          <img
+            className="images"
+            src="/photos/bois2.jpg"
+            alt="bois"
+            onClick={handleClick}
+          />
+          <img
+            className="images"
+            src="/photos/bois3.jpg"
+            alt="bois"
+            onClick={handleClick}
+          />
+          <img
+            className="images"
+            src="/photos/bois8.jpg"
+            alt="bois"
+            onClick={handleClick}
+          />
           {/* <img src="/photos/bois4.jpg" alt='bois' onClick={handleClick} />
           <img src="/photos/bois5.jpg" alt='bois' onClick={handleClick} />
           <img src="/photos/bois6.jpg" alt='bois' onClick={handleClick} /> */}
         </div>
       </>
       {selectedImage && (
-        <div className='overlay' onClick={() => setSelectedImage(null)}>
-          <img src={selectedImage} alt='bois' />
+        <div className="overlay" onClick={() => setSelectedImage(null)}>
+          <img src={selectedImage} alt="bois" />
         </div>
       )}
     </div>
