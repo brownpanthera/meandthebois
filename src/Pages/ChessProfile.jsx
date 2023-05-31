567677c97dbe7aa7ca8582e857fed416bb950178
Ali Faizan <alifaizan1983@gmail.com>
2023-05-31 16:58:25 +0530

    f

--- src/Pages/ChessProfile.jsx
+++ src/Pages/ChessProfile.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from "react";
 import { IoCloseSharp } from "react-icons/io5";
 import { BsFillLightningFill } from "react-icons/bs";
