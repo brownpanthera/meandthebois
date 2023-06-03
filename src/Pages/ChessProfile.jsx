ffe8c5170cd7f5905e338b3875fb014a932bf140
Ali Faizan <alifaizan1983@gmail.com>
2023-06-04 01:37:36 +0530

    fixed avatars sync

--- src/Pages/ChessProfile.jsx
+++ src/Pages/ChessProfile.jsx
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { IoCloseSharp } from "react-icons/io5";
 import { BsFillLightningFill } from "react-icons/bs";
 import { MdOutlineTimer } from "react-icons/md";
@@ -9,8 +8,9 @@ export default function ChessProfile() {
   const [playerData, setPlayerData] = useState([]);
   const [selectedPlayer, setSelectedPlayer] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
   const [isLoadingRatings, setIsLoadingRatings] = useState(false);
+  const [allAvatarsLoaded, setAllAvatarsLoaded] = useState(false);
+  const [isLoadingAvatars, setIsLoadingAvatars] = useState(true); // New state variable for loading avatars
 
   useEffect(() => {
     const playerUrls = [
@@ -25,15 +25,19 @@ export default function ChessProfile() {
 
     const fetchPlayerData = async () => {
       try {
+        setIsLoadingAvatars(true); // Set loading status to true
+
         const playerData = await Promise.allSettled(
           playerUrls.map((url) =>
             fetch(url).then((response) => response.json())
           )
         );
         setPlayerData(playerData.map(({ value }) => value));
-        setIsLoading(false);
+        setAllAvatarsLoaded(true); // Set all avatars loaded
       } catch (error) {
         console.error("Error fetching player data:", error);
+      } finally {
+        setIsLoadingAvatars(false); // Set loading status to false
       }
     };
 
@@ -94,146 +98,147 @@ export default function ChessProfile() {
 
   return (
     <>
-      <div
-        className={`avatar-container ${
-          isOpen ? "avatar-container--hidden" : ""
-        }`}
-      >
-        {playerData.map((player) => {
-  if (player) {
-    const { name, username, avatar, last_online, league, player_id } = player;
-    return (
-      <div key={player_id} className="avatar">
-        {avatar ? (
-          <img
-            onClick={() =>
-              onClickingImage({
-                name,
-                username,
-                last_online,
-                league,
-                player_id,
-                avatar,
-              })
-            }
-            src={avatar}
-            alt={username}
-            width={100}
-            height={100}
-          />
-        ) : (
-          <img
-            onClick={() =>
-              onClickingImage({
-                name,
-                username,
-                last_online,
-                league,
-                player_id,
-                avatar,
-              })
-            }
-            src="./noavatar.gif"
-            width={100}
-            alt={username}
-          />
-        )}
-      </div>
-    );
-  } else {
-    return null; // or a placeholder element
-  }
-})}
-
-      </div>
-
-      {isLoading ? (
+      {isLoadingAvatars ? (
         <div className="outerLoader">
-          <img className="cat" src="pica.gif" />
+          <img className="cat" src="pica.gif"/>
         </div>
       ) : (
-        selectedPlayer && (
-          <div className="modal">
-            {selectedPlayer.avatar ? (
-              <img
-                src={selectedPlayer.avatar}
-                alt={selectedPlayer.username}
-                width={100}
-              />
-            ) : (
-              <img src="./noavatar.gif" width={100} alt="No Avatar" />
-            )}
-            <h2 className="modal_playerName">
-              <span>{selectedPlayer.username}</span>
-            </h2>
-            {selectedPlayer.name ? (
-              <p>{selectedPlayer.name}</p>
-            ) : (
-              <p>
-                <span>bhai name update kar chess[dot]com pe jaake</span>
-              </p>
-            )}
-            <p>
-              League: <span>{selectedPlayer.league}</span>
-            </p>
-            <p>
-              Last Online: <span>{online(selectedPlayer.last_online)}</span>
-            </p>
-            <hr></hr>
-            {isLoadingRatings ? (
-              <div className="catloader">
-                <img src="/pica.gif" width={100} />
-              </div>
-            ) : (
-              <div className="chess_rating">
-                {selectedPlayer.blitz_rating ? (
-                  <div className="rating">
-                    <p className="rating">Blitz</p>
-                    <BsFillLightningFill color="#FEDD00" />
-                    <p className="rating">{selectedPlayer.blitz_rating}</p>
+        <div
+          className={`avatar-container ${
+            isOpen ? "avatar-container--hidden" : ""
+          }`}
+        >
+          {allAvatarsLoaded &&
+            playerData.map((player) => {
+              if (player) {
+                const { name, username, avatar, last_online, league, player_id } =
+                  player;
+                return (
+                  <div key={player_id} className="avatar">
+                    {avatar ? (
+                      <img
+                        onClick={() =>
+                          onClickingImage({
+                            name,
+                            username,
+                            last_online,
+                            league,
+                            player_id,
+                            avatar,
+                          })
+                        }
+                        src={avatar}
+                        alt={username}
+                        width={100}
+                        height={100}
+                      />
+                    ) : (
+                      <img
+                        onClick={() =>
+                          onClickingImage({
+                            name,
+                            username,
+                            last_online,
+                            league,
+                            player_id,
+                            avatar,
+                          })
+                        }
+                        src="./noavatar.gif"
+                        width={100}
+                        alt={username}
+                      />
+                    )}
                   </div>
-                ) : (
-                  <p className="no-rating">
-                    Blitz Rating: <span>0</span>
-                  </p>
-                )}
+                );
+              } else {
+                return null;
+              }
+            })}
+        </div>
+      )}
 
-                {selectedPlayer.rapid_rating ? (
-                  <div className="rating">
-                    <p className="rating">Rapid</p>
-                    <MdOutlineTimer color="#6BF216" />
-                    <p className="rating">{selectedPlayer.rapid_rating}</p>
-                  </div>
-                ) : (
-                  <p className="no-rating">
-                    Rapid Rating: <span>0</span>
-                  </p>
-                )}
+      {selectedPlayer && (
+        <div className="modal">
+          {selectedPlayer.avatar ? (
+            <img
+              src={selectedPlayer.avatar}
+              alt={selectedPlayer.username}
+              width={100}
+            />
+          ) : (
+            <img src="./noavatar.gif" width={100} alt="No Avatar" />
+          )}
+          <h2 className="modal_playerName">
+            <span>{selectedPlayer.username}</span>
+          </h2>
+          {selectedPlayer.name ? (
+            <p>{selectedPlayer.name}</p>
+          ) : (
+            <p>
+              <span>bhai name update kar chess[dot]com pe jaake</span>
+            </p>
+          )}
+          <p>
+            League: <span>{selectedPlayer.league}</span>
+          </p>
+          <p>
+            Last Online: <span>{online(selectedPlayer.last_online)}</span>
+          </p>
+          <hr></hr>
+          {isLoadingRatings ? (
+            <div className="catloader">
+              <img src="/pica.gif" width={100} alt="Loading ratings..." />
+            </div>
+          ) : (
+            <div className="chess_rating">
+              {selectedPlayer.blitz_rating ? (
+                <div className="rating">
+                  <p className="rating">Blitz</p>
+                  <BsFillLightningFill color="#FEDD00" />
+                  <p className="rating">{selectedPlayer.blitz_rating}</p>
+                </div>
+              ) : (
+                <p className="no-rating">
+                  Blitz Rating: <span>0</span>
+                </p>
+              )}
 
-                {selectedPlayer.bullet_rating ? (
-                  <div className="rating">
-                    <p className="rating">Bullet</p>
-                    <GiBulletBill color="#F7F749" />
-                    <p className="rating">{selectedPlayer.bullet_rating}</p>
-                  </div>
-                ) : (
-                  <p className="no-rating">
-                    Bullet Rating: <span>0</span>
-                  </p>
-                )}
-              </div>
-            )}
-            <button
-              className="modal_close_button"
-              onClick={() => {
-                setSelectedPlayer(null);
-                setIsOpen(false);
-              }}
-            >
-              <IoCloseSharp size={30} />
-            </button>
-          </div>
-        )
+              {selectedPlayer.rapid_rating ? (
+                <div className="rating">
+                  <p className="rating">Rapid</p>
+                  <MdOutlineTimer color="#6BF216" />
+                  <p className="rating">{selectedPlayer.rapid_rating}</p>
+                </div>
+              ) : (
+                <p className="no-rating">
+                  Rapid Rating: <span>0</span>
+                </p>
+              )}
+
+              {selectedPlayer.bullet_rating ? (
+                <div className="rating">
+                  <p className="rating">Bullet</p>
+                  <GiBulletBill color="#F7F749" />
+                  <p className="rating">{selectedPlayer.bullet_rating}</p>
+                </div>
+              ) : (
+                <p className="no-rating">
+                  Bullet Rating: <span>0</span>
+                </p>
+              )}
+            </div>
+          )}
+          <button
+            className="modal_close_button"
+            onClick={() => {
+              setSelectedPlayer(null);
+              setIsOpen(false);
+            }}
+          >
+            <IoCloseSharp size={30} />
+          </button>
+        </div>
       )}
     </>
   );
